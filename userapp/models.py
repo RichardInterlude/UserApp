618a03fdc1e2f76d152ffa275cab0b44a078e0e0
Odewale Richard <odewalerichard@gmail.com>
2025-07-12 23:41:56 +0100

    models and register

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+GENDER_CHOICE = (
+    ('M','Male'),
+    ('F','Female'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50, choices = GENDER_CHOICE)
+    profile_pix = models.ImageField(upload_to='user_pix', default='https://thumbs.dreamstime.com/b/user-profile-icon-vector-avatar-person-picture-portrait-symbol-neutral-gender-silhouette-circle-button-photo-blank-272664038.jpg')
+
+    def __str__(self):
+        return self.full_name
