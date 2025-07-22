3d18320325f88e9625ea401365cb9f6de701e7b5
Odewale Richard <odewalerichard@gmail.com>
2025-07-22 03:31:38 +0100

    Third deployment

--- user/settings.py
+++ user/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://userapp-s69q.onrender.com','http://userapp-s69q.onrender.com','userapp-s69q.onrender.com','userapp-s69q.onrender.com']
 
 
 # Application definition
